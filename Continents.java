742e8f4a0b5eac3102dc53d584a9455c95fb8504
Trevor <trevorliu@gmail.com>
2020-12-14 08:53:07 -0800

    jframe?

--- Continents.java
+++ Continents.java
@@ -1,4 +1,4 @@
-//import Java.util.ArrayList;
+import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -6,38 +6,61 @@ import java.awt.event.*;
 public class Continents implements ActionListener{
   JFrame frame;
   JPanel panel;
+  JLabel label;
   JButton button, button2;
 
 
   static int continent = 4;
-  //static ArrayList<String> contList;
+  static ArrayList<String> contList;
+  static JComboBox<ArrayList> mycombo;
 
   public Continents(){
 
-    frame = new JFrame("Poggeur");
+    frame = new JFrame("jframe givs me 100% but drains my wouol");
     panel = new JPanel();
+    label = new JLabel("Welcome to the Continents Program!!");
     button = new JButton("b");
-
-    button = new JButton("b");
-
-
-    button.addActionListener(this);
-    button.addActionListener(this);
     
-    panel.add(button);
+    button2 = new JButton("BB");
+
+
+    button.addActionListener(this);
+    button2.addActionListener(this);
+    
+
+
+
 
     frame.add(panel);
+
+    panel.add(label);
+
+    panel.add(button);
+    panel.add(button2);
 
     frame.setSize(300,300);
     frame.setVisible(true);
   } //END construtor
 
   public static void main(String args[]){
-  //contList = new ArrayList<String>;
-  //contList.add()
+  contList = new ArrayList<String>();
+  contList.add("Choose a Continent!");
+  contList.add("North America");
+  contList.add("South America");
+  contList.add("Europe");
+  contList.add("Africa");
+  contList.add("Asia");
+  contList.add("Australia");
+  contList.add("Antartica");
   System.out.println(getMajorCity(continent));
+
+  
+  Continents inst1 = new Continents();
   }
 
+
+
+  //getMajorCity function
   public static String getMajorCity(int contNumber){
 
     String output;
@@ -77,7 +100,13 @@ public class Continents implements ActionListener{
 
     return output;
     
-  } //END Main
+  } //END function
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    // TODO Auto-generated method stub
+    System.out.println(e.getActionCommand());
+  }
 
   
 } //END Class
